cd5912985a02ecc721a64f14ed11bc14e885b50f
Yisusmr99 <jesus1945.jm@gmail.com>
2020-04-28 09:45:04 -0600

    Setting up our React app

---
+++ src/confiig.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "guia-notes"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://t17oqxun8g.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_xP06eqUYG",
+      APP_CLIENT_ID: "6e19vcakcbdon3ljsfs2ikcehd",
+      IDENTITY_POOL_ID: "us-east-1:97f7b1fd-7837-4208-b03e-3ec45f931f3a"
+    }
+};
